90ce226b8db7dce95a1323c2b3ea76f4683a5447
Pedro Fernandes <pedro.santos@gruporbs.com.br>
2024-09-21 20:48:23 -0300

    🌱 init: start project
    config environment

---
+++ src/test/java/com/simplify/challenge/backend/SimplifyChallengeBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.simplify.challenge.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimplifyChallengeBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
