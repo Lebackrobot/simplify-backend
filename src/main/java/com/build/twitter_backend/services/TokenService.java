bc610226c9b88b32631a873844df62fca522712b
Pedro Fernandes <lebackman18@gmail.com>
2024-10-19 22:55:16 -0300

    📝 refactor: tokenService

--- src/main/java/com/build/twitter_backend/services/TokenService.java
+++ src/main/java/com/build/twitter_backend/services/TokenService.java
@@ -5,6 +5,7 @@ import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.JWTCreationException;
 import com.auth0.jwt.exceptions.JWTVerificationException;
 import com.build.twitter_backend.models.UserModel;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import java.time.Instant;
@@ -13,10 +14,12 @@ import java.time.ZoneOffset;
 
 @Service
 public class TokenService {
+    @Value("${SECRET_KEY}")
+    private String SECRET_KEY;
+    private Algorithm algorithm = Algorithm.HMAC256(SECRET_KEY);
+
     public String generateToken(UserModel user) throws JWTCreationException {
         try {
-            var algorithm = Algorithm.HMAC256("secret");
-
             return JWT.create()
                     .withIssuer("twitter-backend")
                     .withSubject(user.getUsername())
@@ -33,13 +36,12 @@ public class TokenService {
         return LocalDateTime.now().plusHours(2).toInstant(ZoneOffset.of("-03:00"));
     }
 
-    public String getSubject(String jwtToken) {
+    public String getSubject(String token) {
         try {
-            var algorithm = Algorithm.HMAC256("secret");
             return JWT.require(algorithm)
                     .withIssuer("twitter-backend")
                     .build()
-                    .verify(jwtToken)
+                    .verify(token)
                     .getSubject();
         }
 
